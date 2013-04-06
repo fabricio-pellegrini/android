0b8bfaeaaeaf9d73e81395175a6a2dde18bb2190
fabricio.pellegrini@gmail.com <pikachu2107>
2013-04-06 00:43:25 -0300

    Commit Inicial

---
+++ MyMoney/src/com/example/mymoney/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.mymoney;
+
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+	EditText _txtValor;
+	TextView _txtData;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        _txtValor =(EditText)findViewById(R.id.txt_Valor);
+        _txtData =(TextView)findViewById(R.id.txt_Data);
+     
+        
+        _txtData.setText(new SimpleDateFormat("dd/MM/yyyy").format( new Date()));
+        
+        _txtValor.addTextChangedListener(new TextWatcher() {
+			
+        	private String current = "";
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				if(!s.toString().equals(current)){
+					_txtValor.removeTextChangedListener(this);
+
+				       String cleanString = s.toString().replaceAll("[R$,.]", "");
+
+				       double parsed = Double.parseDouble(cleanString);
+				       String formated = NumberFormat.getCurrencyInstance().format((parsed/100));
+
+				       current = formated;
+				       _txtValor.setText(formated);
+				       _txtValor.setSelection(formated.length());
+
+				       _txtValor.addTextChangedListener(this);
+				    }
+				
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void afterTextChanged(Editable s) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
